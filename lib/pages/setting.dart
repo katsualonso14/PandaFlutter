246815743309ada20d42324e41403c64980eb8ed
Shota Onishi <showta.0804@gmail.com>
2022-04-06 09:21:33 +0900

    make setting page (in progress)

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,21 +1,172 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import 'package:image_form_field/image_form_field.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import 'upload_button.dart';
+import 'image_input_adapter.dart';
+
 import 'package:test_flutter/model/appuser.dart';
 
-class SettingPage extends StatefulWidget {
-  const SettingPage({Key? key}) : super(key: key);
-
-  @override
-  _settingPageState createState() => _settingPageState();
-}
-
-class _settingPageState extends State<SettingPage> {
+class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Row(
+      appBar: AppBar(centerTitle: true, title: const Text('Upload Images')),
+      body: SingleChildScrollView(
+        // Provide existing images as the first argument
+        child: SettingForm(<BlogImage>[]),
+      ),
+    );
+  }
+}
+
+class BlogImage {
+  const BlogImage({
+    required this.storagePath,
+    required this.originalUrl,
+    required this.bucketName,
+    this.id,
+  });
+
+  final String storagePath;
+  final String originalUrl;
+  final String bucketName;
+  final String? id;
+
+  static String get collectionPath => 'blogImages';
+
+  Future<void> create() {
+    return FirebaseFirestore.instance.collection(collectionPath).doc().set({
+      'storagePath': storagePath,
+      'originalUrl': originalUrl,
+      'bucketName': bucketName,
+    });
+  }
+
+  static Future<BlogImage?> fromUrl(String url) async {
+    final images = await FirebaseFirestore.instance
+        .collection(collectionPath)
+        .where('originalUrl', isEqualTo: url)
+        .get();
+
+    if (images.docs.isNotEmpty) {
+      final i = images.docs.first.data();
+
+      return BlogImage(
+        storagePath: i['storagePath'],
+        originalUrl: i['originalUrl'],
+        bucketName: i['bucketName'],
+        id: images.docs.first.id,
+      );
+    }
+
+    return null;
+  }
+
+  Future delete() async {
+    await FirebaseStorage.instance.ref().child(storagePath).delete();
+    return FirebaseFirestore.instance.collection(collectionPath).doc(id).delete();
+  }
+}
+
+class SettingForm extends StatefulWidget {
+  SettingForm(this.existingImages);
+
+  final List<BlogImage> existingImages;
+
+  @override
+  State<StatefulWidget> createState() => SettingFormState();
+}
+
+class SettingFormState extends State<SettingForm> {
+  final _formKey = GlobalKey<FormState>();
+  List<ImageInputAdapter>? _images;
+
+  void submit() {
+    if (_formKey.currentState?.validate() ?? false) {
+      _formKey.currentState?.save();
+      var snackbarText = 'Upload successful';
+
+      try {
+        // New images
+        _images?.where((i) => i.isFile).forEach((i) async {
+          final photo = await i.save();
+
+          await BlogImage(
+            storagePath: photo.refPath,
+            originalUrl: photo.originalUrl,
+            bucketName: photo.bucketName,
+          ).create();
+        });
+
+        // Removed images
+        widget.existingImages
+            .where((r) => !(_images?.any((m) => m.url == r.originalUrl) ?? false))
+            .forEach((i) {
+          BlogImage.fromUrl(i.originalUrl).then((b) => b?.delete());
+        });
+      } catch (e) {
+        print(e);
+        snackbarText = "Couldn't save. Please try again later.";
+      } finally {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(snackbarText)));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final shouldAllowMultiple = true;
+
+    return Form(
+      key: _formKey,
+      child: ListBody(
         children: [
-          Text('Setting'),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Display Name',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Comment',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          ImageFormField<ImageInputAdapter>(
+            shouldAllowMultiple: shouldAllowMultiple,
+            onSaved: (val) => _images = val,
+            initialValue: widget.existingImages
+                .map((i) => ImageInputAdapter(url: i.originalUrl))
+                .toList()
+                .cast<ImageInputAdapter>(),
+            initializeFileAsImage: (file) => ImageInputAdapter(
+              file: UploadableImage(
+                file,
+                storagePath: 'appImages',
+              ),
+            ),
+            buttonBuilder: (_, count) =>
+                PhotoUploadButton(count: count, shouldAllowMultiple: shouldAllowMultiple),
+            previewImageBuilder: (_, image) => image.widgetize(),
+          ),
+          TextButton(
+            onPressed: submit,
+            child: const Text('Update Profile'),
+          )
         ],
       ),
     );
