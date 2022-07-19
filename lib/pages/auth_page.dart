2b721f9e69283f5c6c61f847351b59deb27db8d5
Shota Onishi <showta.0804@gmail.com>
2022-07-20 00:03:40 +0900

    delete create page

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -28,53 +28,6 @@ class _AuthPage extends State<AuthPage> {
           child: Column(
             children: <Widget>[
               TextFormField(
-                // テキスト入力のラベルを設定
-                decoration: InputDecoration(labelText: "メールアドレス"),
-                onChanged: (String value) {
-                  setState(() {
-                    newUserEmail = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
-                // パスワードが見えないようにする
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    newUserPassword = value;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  try {
-                    // メール/パスワードでユーザー登録
-                    final FirebaseAuth auth = FirebaseAuth.instance;
-                    final UserCredential result =
-                        await auth.createUserWithEmailAndPassword(
-                      email: newUserEmail,
-                      password: newUserPassword,
-                    );
-
-                    // 登録したユーザー情報
-                    final User user = result.user!;
-                    setState(() {
-                      infoText = "登録OK：${user.email}";
-                    });
-                    Navigator.of(context).pushNamed("/settingpage");
-                  } catch (e) {
-                    // 登録に失敗した場合
-                    setState(() {
-                      infoText = "登録NG：${e.toString()}";
-                    });
-                  }
-                },
-                child: Text("ユーザー登録"),
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
                 decoration: InputDecoration(labelText: "メールアドレス"),
                 onChanged: (String value) {
                   setState(() {
@@ -107,7 +60,6 @@ class _AuthPage extends State<AuthPage> {
                     setState(() {
                       infoText = "ログインOK：${user.email}";
                     });
-                    Navigator.of(context).pushNamed("/settingpage");
                   } catch (e) {
                     // ログインに失敗した場合
                     setState(() {
