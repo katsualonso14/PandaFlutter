9562e61b4894209b6b179944dba0350b5b00dc60
katsualonso14 <xabi.k0124football@gmail.com>
2025-04-30 15:25:38 +0900

    Change State Watch Provider from staful in Navigation.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -35,7 +34,7 @@ class MyApp extends ConsumerWidget {
               error: (error, stackTrace) =>
               const Center(child: Text('ユーザーデータエラー')),
               data: (_) {
-                return Navigation();
+                return const Navigation();
               },
             );
           },
--- lib/view/parts/navigation.dart
+++ lib/view/parts/navigation.dart
@@ -1,63 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:test_flutter/view/pages/laundry_post_page.dart';
 import 'package:test_flutter/view/pages/post_page.dart';
 import 'package:test_flutter/view/pages/todo_page.dart';
 import 'package:test_flutter/view/parts/my_bottom_nav_bar.dart';
 import 'package:test_flutter/view/parts/welcome_page.dart';
-import 'package:test_flutter/view_model/pref_view_model.dart';
+import 'package:test_flutter/view_model/app_state_provider.dart';
 
-class Navigation extends StatefulWidget {
-  @override
+class Navigation extends ConsumerWidget {
   const Navigation({Key? key}) : super(key: key);
-
   @override
-  _NavigationState createState() => _NavigationState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firstLaunch = ref.watch(firstLaunchProvider);
+    final bottomNavIndex = ref.watch(bottomNavIndexProvider);
 
-class _NavigationState extends State<Navigation> {
-  var _currentIndex  = 0;
-  bool _isFirstLaunch = false;
-  final _prefViewModel = PrefViewModel();
-
-  @override
-  void initState() {
-    super.initState();
-    _checkFirstLaunch();
-  }
-
-  Future<void> _checkFirstLaunch() async {
-    bool isFirstLaunch = await _prefViewModel.checkFirstLaunch();
-    setState(() {
-      _isFirstLaunch = isFirstLaunch;
-    });
-  }
-
-  void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: !_isFirstLaunch
-          ? IndexedStack(
-              index: _currentIndex,
+    return firstLaunch.when(
+        data: (isFirstLaunch) {
+          return Scaffold(
+            body: isFirstLaunch ?
+            const WelcomePage() :
+            IndexedStack(
+              index: bottomNavIndex,
               children: const [
                 PostPage(),
                 LaundryPostPage(),
                 TodoPage(),
               ],
-            )
-          : WelcomePage(
-              onStartPressed: () {
-                setState(() {
-                  _isFirstLaunch = false;
-                });
-              },
             ),
-      bottomNavigationBar: MyBottomNavBar(currentIndex: _currentIndex, onTap: _onTap),
+            bottomNavigationBar: const MyBottomNavBar(),
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => const Center(child: Text('Error loading first launch state'))
     );
   }
 }
